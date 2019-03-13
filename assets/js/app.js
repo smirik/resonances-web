842144bd5c90dbe56547a83d55dbce5760c85417
smirik <smirik@gmail.com>
2019-03-13 13:48:45 +0300

    WIP

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,13 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.scss');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+require('../css/bootstrap.min.css');
 
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
+// or you can include specific pieces
 require('bootstrap');
+// require('bootstrap/js/dist/popover');
 
-global.$ = global.jQuery = $;
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
